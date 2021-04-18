78607988080ed95ab1477e788e425096404bfc26
JUAN NICOLAS MARROQUIN ALFARO <jnmarroquin@itc.com.co>
2021-04-17 19:39:33 -0500

    Inicio del proyecto

---
+++ src/main/java/com/despegar/hoteles/OrquestacionHotelesApplication.java
@@ -0,0 +1,13 @@
+package com.despegar.hoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestacionHotelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestacionHotelesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/despegar/hoteles/OrquestacionHotelesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despegar.hoteles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestacionHotelesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
