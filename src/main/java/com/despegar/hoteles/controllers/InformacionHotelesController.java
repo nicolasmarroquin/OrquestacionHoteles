ff7b8208c5e4f66959b6b41cec62952f6a22af80
JUAN NICOLAS MARROQUIN ALFARO <jnmarroquin@itc.com.co>
2021-04-23 20:26:56 -0500

    Se añade anotacion crossorigin

--- src/main/java/com/despegar/hoteles/controllers/InformacionHotelesController.java
+++ src/main/java/com/despegar/hoteles/controllers/InformacionHotelesController.java
@@ -2,6 +2,7 @@ package com.despegar.hoteles.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.despegar.hoteles.models.entity.Hotel;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class InformacionHotelesController {
